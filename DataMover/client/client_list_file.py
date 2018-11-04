057d296c3ac48bfb953acc05eafde9d0ef6a0591
Nehir Poyraz <poyrazf@mef.edu.tr>
2018-11-04 21:16:34 +0300

    client test

--- DataMover/client/client_list_file.py
+++ DataMover/client/client_list_file.py
@@ -3,24 +3,31 @@
 # DataMover
 #
 # Created by Nehir Poyraz on 28.10.2018
-# Copyright © 2018 Nehir Poyraz. All rights reserved.
+# Copyright  2018 Nehir Poyraz. All rights reserved.
 
 import sys
 import os
+from socket import *
+PORT = 1111
+HOST = '10.0.2.15'
+ADDR = (HOST, PORT)
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect(ADDR)
 
 
-def list(directory):
-    """
-    :param directory:
-    :return:
-    """
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            os.listdir(directory)
-    except OSError:
-        print('Error: Creating directory. ' + directory)
+def list():
+    NotImplemented
 
 
 if __name__ == '__main__':
-    list()
+    sock.send("Connected")
+    print("Connected from:", sock.getsockname())
+    print("Connected to:", sock.getpeername())
+    while True:
+        print("waiting")
+        result = sock.recv(1024)
+        print(result)
+        sock.send('foo')
+        print("Sent: foo")
+        if result == 3:
+            break
