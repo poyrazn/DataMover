d525107064f85156d8f7d7f11fbebc6cccdc5ce3
Nehir Poyraz <poyrazf@mef.edu.tr>
2018-11-11 23:26:25 +0300

    variable names changed reformat

--- server/server_xinetd_list.py
+++ server/server_xinetd_list.py
@@ -5,25 +5,22 @@
 #
 # Created by Nehir Poyraz on 10.11.2018
 
+from __future__ import print_function
 import os
 import sys
 
+
 if __name__ == '__main__':
 
-    buffer = sys.stdin.readline().strip()
-    username = ''
-    if len(buffer):
-        username += buffer
-    path = '/home/DataCloud/' + username
-    if not os.path.isdir(path):
-        os.mkdir(path)
-        print('Data directory is created.')
-        sys.stdout.flush()
-    else:
-        if not os.listdir(path):
-            print('-' * 70 + '\n\033[91m There are no stored files\033[0m\n' + '-' * 70)
-            sys.stdout.flush()
+    username = sys.stdin.readline().strip()
+    if len(username):
+        path = '/home/DataCloud/' + username
+        if not os.path.isdir(path):
+            os.mkdir(path)
+            print("Data directory is created.", flush=True)
         else:
-            os.system('ls -la ' + path)
-            sys.stdout.flush()
-     # Flush the standard output, so the message is sent.
+            if not os.listdir(path):
+                print('-' * 70 + '\n\033[91m There are no stored files\033[0m\n' + '-' * 70, flush=True)
+            else:
+                os.system('ls -la ' + path)
+                sys.stdout.flush()
---
+++ server/server_xinetd_put.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python2.7
+#
+# server_xinetd_put
+# DataMover
+#
+# Created by Nehir Poyraz on 11.11.2018
+
+from __future__ import print_function
+import os
+import time
+import sys
+import hashlib
+
+if __name__ == '__main__':
+    md5 = hashlib.md5()
+    username = sys.stdin.readline().strip()
+    path = '/home/DataCloud/' + username + '/'
+    filename = sys.stdin.readline().strip()
+    filepath = path + filename
+    clientmd5 = sys.stdin.readline().strip()
+    filesize = sys.stdin.readline().strip()
+    content = sys.stdin.read()
+
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            md5.update(f.read())
+        print('\033[93mFile' + filename + ' exists. Calculating MD5 checksum.\033[0m', flush=True)
+        if clientmd5 == md5.digest():
+            print('\033[92mFile ' + filename + ' is succesfully recorded. MD5 checksum passed.\033[0m', flush=True)
+        else:
+            print('\033[91mFile is either corrupted, interrupted or outdated.\033[0m', flush=True)
+
+    else:
+        print('\033[94m' + filename + ' is being stored...', flush=True)
+        starttime = time.ctime()
+        f = open(filepath, 'w')
+        endtime = time.ctime()
+        for i in range(100):
+            print ('=', end='', flush=True)
+        print('100 % Complete! (', endtime-starttime, 'secs )\033[0m', flush=True)
+        f.write(content)
+        f.close()
+        with open(filepath) as f:
+            md5.update(f.read())
+        if clientmd5 == md5.digest():
+            print('\033[92mFile ' + filename + ' is succesfully recorded. MD5 checksum passed.\033[0m', flush=True)
+        else:
+            print('\033[91mTransmission failed. Retry recommended.\033[0m', flush=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
