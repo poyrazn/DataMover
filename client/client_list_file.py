69a084b54d165cdb9651a180f4f51381fefdc4f9
Nehir Poyraz <poyrazf@mef.edu.tr>
2018-11-11 18:24:06 +0300

    client-script list directory

--- client/client_list_file.py
+++ client/client_list_file.py
@@ -5,17 +5,23 @@
 # Created by Nehir Poyraz on 10.11.2018
 
 import socket
+import os
 import sys
 
-HOST = '10.0.2.15'
+#HOST = '10.0.2.15'
+HOST = 'localhost'
 PORT = 1111
 ADDR = (HOST, PORT)
 BUFSIZE = 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(ADDR)
+machine = socket.gethostname()
 
 
 if __name__ == '__main__':
+    user = os.getlogin()
+    sock.send(user)
+    sock.shutdown(socket.SHUT_WR)
     message = input("Message to send: ")
     sock.send(message)
     sock.shutdown(socket.SHUT_WR)
