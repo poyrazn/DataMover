3d14e53b2ace38296280ca7be71176cb57c63e78
Nehir Poyraz <poyrazf@mef.edu.tr>
2018-11-12 03:23:58 +0300

    correction

--- client/client_put_file.py
+++ client/client_put_file.py
@@ -5,16 +5,21 @@
 #
 # Created by Nehir Poyraz on 11.11.2018
 
+#!/usr/bin/python2.7
+#
+# client_put_file
+# DataMover
+#
+# Created by Nehir Poyraz on 11.11.2018
+
 from __future__ import print_function
 import socket
 import os
 import sys
 import hashlib
-import pathlib
-
 
 HOST = 'localhost'
-PORT = 1111
+PORT = 2222
 ADDR = (HOST, PORT)
 BUFSIZE = 4096
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,29 +29,26 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('Please provide a file to be transferred.')
     else:
-        filename = pathlib.Path(sys.argv[1]).name
+        filename = sys.argv[1]
         filepath = os.getcwd() + '/' + filename
-        md5 = hashlib.md5()
-        filesize = os.path.getsize(filepath)
-        username = os.getlogin()
-        sock.send(username)
-        sock.send(filename)
-        status = sock.recv(BUFSIZE)
-        print(status)
-        """sock.send(md5.digest())
-        sock.send(str(filesize))
-        #sock.send(content)
-        sock.shutdown(socket.SHUT_WR)
-        status = sock.recv(BUFSIZE)
-        print(status)"""
-
-        """if os.path.exists(filepath):
+        if os.path.exists(filepath):
             md5 = hashlib.md5()
             with open(filepath) as f:
                 content = f.read()
-                md5.update(content.encode('utf-8'))
-            
-            
-            
+                md5.update(content)
+            filesize = os.path.getsize(filepath)
+            username = os.getlogin()
+            sock.send(username)
+            sock.send(filename)
+            sock.send(md5.digest())
+            sock.send(str(filesize))
+            sock.send(content)
+            sock.shutdown(socket.SHUT_WR)
+            status = sock.recv(BUFSIZE)
+            print(status)
         else:
-            print('File does not exists.')"""
+            print('File does not exists.')
+
+
+
+
