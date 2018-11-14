b59a8ea890b19250910a293b51af99a1f157fc88
nehir <poyrazf@mef.edu.tr>
2018-11-14 15:08:22 +0100

    log

--- server/server_xinetd_list.py
+++ server/server_xinetd_list.py
@@ -10,6 +10,10 @@ import os
 import sys
 import logging
 
+FORMAT ='%(asctime)-15s | %(levelname)s: %(message)s'
+logging.basicConfig(filename='home/DataCloud/server.log', level=logging.DEBUG, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
 
     username = sys.stdin.readline().strip()
@@ -17,11 +21,17 @@ if __name__ == '__main__':
         path = '/home/DataCloud/' + username
         if not os.path.isdir(path):
             os.mkdir(path)
-            sys.stdout.write('202, Data directory ' + path + ' is created.')
+            message = 'Data directory ' + path + ' is created.'
+            sys.stdout.write('202,' + message )
+            logger.info(message)
         else:
             if not os.listdir(path):
                 #print('204, Data directory is empty.')
-                sys.stdout.write('204, Data directory is empty.')
+                message = 'Directory ' + path + ' is empty.'
+                sys.stdout.write('204,' + message)
+                logger.info(message)
             else:
                 os.system('ls -la ' + path)
+                message = 'Directory ' + path + ' is listed.'
+                logger.info(message)
 		sys.stdout.flush()
