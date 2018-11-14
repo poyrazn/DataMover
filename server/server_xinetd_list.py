dbfacceb55aba276fc7390f2c71f16688934679d
nehir <poyrazf@mef.edu.tr>
2018-11-14 14:55:08 +0100

    working

--- server/server_xinetd_list.py
+++ server/server_xinetd_list.py
@@ -5,7 +5,7 @@
 #
 # Created by Nehir Poyraz on 10.11.2018
 
-from __future__ import print_function
+
 import os
 import sys
 import logging
@@ -17,10 +17,11 @@ if __name__ == '__main__':
         path = '/home/DataCloud/' + username
         if not os.path.isdir(path):
             os.mkdir(path)
-            print("Data directory is created.", flush=True)
+            sys.stdout.write('202, Data directory ' + path + ' is created.')
         else:
             if not os.listdir(path):
-                print('-' * 70 + '\n\033[91m There are no stored files\033[0m\n' + '-' * 70, flush=True)
+                #print('204, Data directory is empty.')
+                sys.stdout.write('204, Data directory is empty.')
             else:
                 os.system('ls -la ' + path)
-                sys.stdout.flush()
+		sys.stdout.flush()
