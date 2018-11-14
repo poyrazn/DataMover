0f19b865cc7f032b2f89795829b9aa784c634679
nehir <poyrazf@mef.edu.tr>
2018-11-14 14:54:01 +0100

    logging

--- client/client_list_file.py
+++ client/client_list_file.py
@@ -8,29 +8,63 @@
 import socket
 import os
 import logging
+import pickle
 
-username = os.getlogin()
+
 FORMAT ='%(asctime)-15s | %(levelname)s: %(message)s'
 logging.basicConfig(filename='client.log', level=logging.DEBUG, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
 logger = logging.getLogger(__name__)
-logger.info('First message')
-print(logger)
+
+BUFSIZE = 4096
+
+RED = '\033[91m'	# fail
+GREEN = '\033[92m'	# success
+LIGHTBLUE = '\033[96m'	# file already exists
+YELLOW = '\033[93m'	# request sent
+END = '\033[0m'
 
 # establish connection
 HOST = 'localhost'
 PORT = 1111
 ADDR = (HOST, PORT)
 BUFSIZE = 4096
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(ADDR)
-log = 'Connected to server.'
 
-# handle request
+def connect():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect(ADDR)
+	return sock
 
+def receive(sock):
+	pickled = sock.recv(BUFSIZE)
+	pickled = b'' + pickled
+	reply = pickle.loads(pickled)
+	sock.shutdown(socket.SHUT_RD)
+	return reply
+
+def check(reply):
+	if reply[:3] == '202' or reply[:3] == '204':
+		reply = reply.split(',')
+		message = reply[1]
+		if reply[0] == '202':
+			logger.info(message)
+			print(GREEN + message + END)
+		else:
+			print(reply[0])
+			logger.info(message)
+			print('-' * 70 + RED + '\nThere are no stored files.\n' + END + '-' * 70 )
+	else:
+		logger.info('Data directory is listed.')
+		print(reply)
+
+sock = connect()
+logger.info('PUT Connected to server.')
+username = os.getlogin()
 sock.send(username)
 sock.shutdown(socket.SHUT_WR)
-data = sock.recv(BUFSIZE)
-print(data)
+reply = sock.recv(BUFSIZE)
+check(reply)
+
+
 
 
 
